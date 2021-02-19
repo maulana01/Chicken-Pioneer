28d918e9f48320c720b991344146613e133f0173
rizalshmbg <sihombingrizal18@gmail.com>
2021-02-20 00:04:50 +0700

    mengubah ke file php

---
+++ thanks.php
@@ -0,0 +1,24 @@
+<?php
+
+header( "refresh:3;url=index.php" );
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Terima Kasih</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+<body>
+    <div class="loading-page">
+        <h1 class="text-center">Terima Kasih Telah Membeli Produk Kami</h1>
+        <br>
+        <h3 class="loading text-center">Anda akan di arahkan ke dalam Landing Page setelah 3 detik</h3>
+    </div>
+</body>
+</html>
